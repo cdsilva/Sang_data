fd60b6a2c72c61a019d81f6c7d0d1c41a89dd656
Carmeline Dsilva <cdsilva@della3.(none)>
2014-02-10 13:39:14 -0500

    Read in folded structure

--- read_data.py
+++ read_data.py
@@ -21,11 +21,14 @@ def read_coordinates(filename, out_mat_filename):
     
         pos = np.zeros((n, dim))
         atom_type = [[] for i in range(n)]
+        residue_ind = [[] for i in range(n)]
         
         for i in range(n):
             line = f.readline()
             ind = int(line.split()[2]) - 1
             atom_type[ind] = line.split()[1]
+            residue_ind[ind] = int(line.split()[0][0:-3])
+            
             for j in range(dim):
                 pos[ind][j] = float(line.split()[3+j])
 
@@ -53,8 +56,33 @@ def read_coordinates(filename, out_mat_filename):
     np.save('pos', np.array(all_pos))
     np.save('isH', isH)
     np.save('isCA', isCA)
+    np.save('residue_ind', np.array(residue_ind))
+            
+    scipy.io.savemat(out_mat_filename,{'pos':np.array(all_pos[::mat_stride]), 't':np.array(t[::mat_stride]), 'box':np.array(all_box[::mat_stride]), 'isH':isH, 'isCA':isCA, 'residue_ind':residue_ind})
+
+def read_folded():
+
+    filename = 'original_folded.gro'
     
-    scipy.io.savemat(out_mat_filename,{'pos':np.array(all_pos[::mat_stride]), 't':np.array(t[::mat_stride]), 'box':np.array(all_box[::mat_stride]), 'isH':isH, 'isCA':isCA})
+    dim = 3
+    
+    f = open(filename, 'r')
+
+    line = f.readline()
+    n = int(f.readline())
+    
+    pos = np.zeros((n, dim))
+    for i in range(n):
+        line = f.readline()
+        ind = int(line.split()[2]) - 1
+                    
+        for j in range(dim):
+            pos[ind][j] = float(line.split()[3+j])
+
+    f.close()
+
+    np.save('folded_structure', pos)
+    scipy.io.savemat('folded_structure.mat',{'folded_pos':pos})
 
 def read_rmsd(filename, output_filename):
     f = open(filename, 'r')
@@ -84,4 +112,5 @@ if __name__ == '__main__':
     rmsd_output_filename = 'rmsd_data.mat'
     
     read_coordinates(filename, output_filename)
+    read_folded()
     read_rmsd(rmsd_filename, rmsd_output_filename)
