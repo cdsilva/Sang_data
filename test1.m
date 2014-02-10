f7b17c9d2693c65aa31f11b4daea9ff3ecb7fde4
Carmeline Dsilva <cdsilva@matthew-remillards-macbook-pro.local>
2014-02-09 22:28:29 -0500

    Updated stest file to include PCA

--- test1.m
+++ test1.m
@@ -1,19 +1,54 @@
 clear all
 close all
 
-%%
+%% load data
+load rmsd_data
 load all_atom_data
 
 nsteps = length(t);
 
 pos = reshape_pos(pos, nsteps);
 
-pos = pos(logical(1-isH'), :, :);
-%pos = pos(logical(isCA)', :, :);
+%pos = pos(logical(1-isH'), :, :);
+pos = pos(logical(isCA)', :, :);
 
 pos = mean_center(pos);
 
-%%
+rg = find_rg(pos);
+
+%% PCA
+pca_data = zeros(nsteps, size(pos,1) * size(pos,2));
+template= pos(:, :, end);
+
+for i=1:nsteps
+    P = pos(:,:,i);
+    Pnew = Kabsch(P, template);
+    pca_data(i,:) = reshape(Pnew, 1, []);
+end
+
+pca_data = pca_data - repmat(mean(pca_data), nsteps, 1);
+
+[V, D] = PCA(pca_data, 10);
+
+for i=1:3
+    figure; 
+    plot(t, pca_data*V(:,i),'.')
+end
+
+for i=1:3
+    figure; 
+    plot(rg, pca_data*V(:,i),'.')
+end
+
+for i=1:3
+    figure; 
+    plot(rmsd, pca_data*V(:,i),'.')
+end
+
+
+return
+
+%% construct distance matrix
 W = zeros(nsteps);
 for i=1:nsteps
     i
@@ -27,12 +62,12 @@ for i=1:nsteps
     end
 end
 
-%%
-eps = 0.25 * median(W(:));
+%% dmaps
+eps = median(W(:));
 
 [V, D] = dmaps(W, eps, 10);
 
-%%
+%% plots
 figure;
 plot(diag(D),'.')
 
@@ -45,7 +80,6 @@ plot(t, V(:,3),'.')
 figure;
 plot3(V(:,2),V(:,3),V(:,4),'.')
 
-rg = find_rg(pos);
 
 figure; 
 plot(t, rg)
@@ -56,3 +90,8 @@ plot(rg, V(:,2), '.')
 figure; 
 plot(rg, V(:,3), '.')
 
+figure;
+plot(rmsd, V(:,2),'.')
+
+figure;
+plot(rmsd, V(:,3),'.')
